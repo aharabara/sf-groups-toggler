bbdd9c3fa41dffc3560745bd20eb0c9bc6a96c2f
aharabara <aharabara@pentalog.com>
2024-01-30 12:12:52 +0200

    latest

--- parse.php
+++ parse.php
@@ -1,27 +1,157 @@
 <?php
 
-require_once "./vendor/autoload.php";
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter\Standard;
 
-$parser = (new \PhpParser\ParserFactory())->createForHostVersion();
+require_once __DIR__ . "/vendor/autoload.php";
 
-$ast = $parser->parse(file_get_contents("./entity.php"));
+$parser = (new ParserFactory())->createForHostVersion();
+
+$path = $argv[1];
+$expectedProp = $argv[2];
+$groups = explode(",", $argv[3]);
+
+assert(file_exists($path), 'File does not exists.');
+
+$oldAst = $parser->parse(file_get_contents($path));
+$oldTokens = $parser->getTokens();
 
 
-$traverser = new \PhpParser\NodeTraverser();
-$traverser->addVisitor(new class extends \PhpParser\NodeVisitorAbstract {
-    public function enterNode(\PhpParser\Node $node) {
-        if ($node instanceof \PhpParser\Node\Stmt\Property){
-//            print $node-.PHP_EOL;
-            foreach ($node->attrGroups as $group){
-                foreach ($group->attrs as $attr){
-                    if (str_contains($attr->name, "Groups")){
-                        print_r($attr);
-                    }
-                }
-            }
-            die;
+$traverser = new NodeTraverser();
+
+$traverser->addVisitor(new \PhpParser\NodeVisitor\CloningVisitor());
+$newAst = $traverser->traverse($oldAst);
+
+
+$traverser->addVisitor(new class ($expectedProp, $groups) extends NodeVisitorAbstract {
+    private \PhpParser\BuilderFactory $factory;
+    private ?Property $currentProperty = null;
+    private Standard $printer;
+    private bool $groupWasModified = false;
+
+    public function __construct(
+        protected $expectedProp,
+        protected $groups
+    )
+    {
+        $this->printer = new Standard();
+        $this->factory = new \PhpParser\BuilderFactory();
+
+        if (empty($this->expectedProp)) {
+            throw new RuntimeException('Property expected.');
+        }
+        if (empty($this->groups)) {
+            throw new RuntimeException('Serialization groups expected.');
         }
     }
-});
 
-$ast = $traverser->traverse($ast);
+
+    public function enterNode(Node $node)
+    {
+        if ($this->groupWasModified) {
+            return NodeVisitorAbstract::STOP_TRAVERSAL;
+        }
+        if ($node instanceof Property && $this->currentProperty === null) {
+            $propertyName = $node->props[0]->name;
+            if ($propertyName->name !== $this->expectedProp) {
+                return \PhpParser\NodeVisitor::DONT_TRAVERSE_CHILDREN;
+            }
+            if (count($node->props) > 1) {
+                throw new RuntimeException('Multiple property cases not handled yet.');
+            }
+            $this->currentProperty = $node;
+        }
+
+
+        return $node;
+    }
+
+    public function leaveNode(Node $node): Node|Node\Attribute|Property|int|null
+    {
+        if ($node instanceof Node\Attribute) {
+            //            print $this->printer->prettyPrint([$node]) . PHP_EOL;
+            return $this->handleAttribute($node);
+        }
+        if ($node instanceof Node\AttributeGroup) {
+            if(empty($node->attrs)) {
+                return NodeVisitor::REMOVE_NODE;
+            }
+        }
+
+        if ($node === $this->currentProperty && !$this->groupWasModified) {
+            $node->attrGroups[] = new Node\AttributeGroup([
+                $this->factory->attribute('Groups', [$this->groups])
+            ]);
+            return $node;
+        }
+
+        return $node;
+    }
+
+    /**
+     * @param Node\Attribute $node
+     * @return int|Node\Attribute
+     */
+    public function handleAttribute(Node\Attribute $node): int|Node\Attribute
+    {
+        if (!str_contains($node->name, "Groups")) {
+            return $node;
+        }
+        $expr = $node->args[0];
+        if (!$expr->value instanceof Array_) {
+            throw new RuntimeException('Serializer groups should be specified in an array.');
+        }
+
+        if ($this->attributeHasGroup($node, $this->groups)) {
+            // @note support constants
+            $expr->value->items = array_filter($expr->value->items, function (Node\ArrayItem $item) {
+                assert($item->value instanceof String_, 'Group not a string.');
+                return !in_array($item->value->value, $this->groups);
+            });
+            if (empty($expr->value->items)) {
+                $this->groupWasModified = true;
+                return NodeVisitor::REMOVE_NODE;
+            }
+        } else {
+            array_push($expr->value->items, ...array_map($this->factory->val(...), $this->groups));
+        }
+        //        $argsAsStr = $this->printer->prettyPrint($node->args);
+        //        print "> {$this->expectedProp} #{$node->name}($argsAsStr)" . PHP_EOL;
+
+        $this->groupWasModified = true;
+
+        return $node;
+    }
+
+    /**
+     * @return true
+     */
+    public function attributeHasGroup(Node\Attribute $attribute, array $groups): bool
+    {
+        foreach ($attribute->args as $arg) {
+            assert($arg->value instanceof Array_);
+            foreach ($arg->value->items as $item) {
+                assert($item->value instanceof String_, 'Only "string" groups are supported.');
+                if (in_array($item->value->value, $groups)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+});
+$prettyPrinter = new Standard();
+
+$oldAst = $traverser->traverse($oldAst);
+
+//print $prettyPrinter->prettyPrint($oldAst, $oldAst, $oldTokens);
+file_put_contents($path, $prettyPrinter->printFormatPreserving($oldAst, $oldAst, $oldTokens));
+//`php-cs-fixer fix $path &>/dev/null`;
